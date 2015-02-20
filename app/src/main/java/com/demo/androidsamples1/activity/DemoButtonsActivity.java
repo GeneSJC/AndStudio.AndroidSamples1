b7aa251cfe3a5e651c055e5d30554809f418e0c6
gene-sbay <geneb.contact@gmail.com>
2015-02-20 12:48:37 -0800

    Initial commit

---
+++ app/src/main/java/com/demo/androidsamples1/activity/DemoButtonsActivity.java
@@ -0,0 +1,104 @@
+package com.demo.androidsamples1.activity;
+
+import android.app.Activity;
+import android.graphics.drawable.TransitionDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.demo.androidsamples1.util.Util;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class DemoButtonsActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_demo_buttons);
+
+		uiDemoButtons();
+		backendDemoButtons();
+	}
+
+	// =======================================
+	// BUTTON LOAD LOGIC
+
+	private void uiDemoButtons()
+	{
+		final Button toast1Button = (Button) findViewById(R.id.toast1_button);
+		toast1Button.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View v)
+			{
+				Util.myToast(v, "Simple toast");
+
+				LinearLayout layout = (LinearLayout) findViewById(R.id.layout_top);
+				layout.setBackgroundResource(R.drawable.translate);
+				TransitionDrawable transition = (TransitionDrawable) layout.getBackground();
+				transition.startTransition(5000);
+			}
+		});
+
+		connectButtonToActivity (R.id.list_button, DemoListActivity.class );
+		connectButtonToActivity (R.id.tabs_button, DemoTabsActivity.class);
+		connectButtonToActivity (R.id.drawables_gradient_button, DrawableDragDropActivity.class);
+
+		Bundle paramsBundle = new Bundle();
+		paramsBundle.putBoolean(Util.DRAGDROP_BOOLEAN_KEY, true);
+		connectButtonToActivity (R.id.drawables_dragdrop_button, DrawableDragDropActivity.class, paramsBundle);
+		
+		connectButtonToActivity(R.id.circle_drawing_activity_button, CircleDrawingActivity.class);
+		connectButtonToActivity(R.id.dnd_with_util_btn, DrawableActivity.class);
+	}
+
+	private void connectButtonToActivity(int androidResourceId, final Class activityClass)
+	{
+		connectButtonToActivity(androidResourceId, activityClass, null);
+	}
+
+	private void connectButtonToActivity(int androidResourceId, final Class activityClass, final Bundle bundle)
+	{
+		final Button button = (Button) findViewById(androidResourceId);
+		button.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View v) {
+
+				Util.goToActivity(v, activityClass, bundle);
+			}
+		});
+
+	}
+
+	private void backendDemoButtons()
+	{
+
+		final Button readFileButton = (Button) findViewById(R.id.readfile_button);
+		readFileButton.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View v)
+			{
+			    try
+			    {
+			    		InputStream inputStream = getAssets().open("input1.txt");
+			    		String fileContents = Util.testReadTextFile(inputStream);
+			    		String msg = "read file contents = " + fileContents;
+			    		Log.d("OK", msg);
+			    		Util.myToast(v, msg);
+			    }
+			    catch (final IOException e)
+			    {
+			    		e.printStackTrace();
+			    		Log.d("ex", e.toString());
+			    }
+			}
+		});
+	}
+}
+
+
---
+++ app/src/main/java/com/demo/androidsamples1/activity/DrawableDragDropActivity.java
@@ -0,0 +1,121 @@
+package com.demo.androidsamples1.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.demo.androidsamples1.util.Util;
+
+public class DrawableDragDropActivity extends Activity
+{
+	private View selected_item = null;
+	private int offset_x = 0;
+	private int offset_y = 0;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_demo_background);
+
+		boolean enableDragDrop = isDragDropEnabled();
+		if (!enableDragDrop)
+		{
+			Util.debug1("enableDragDrop is false, so returning from activity ");
+			return;
+		}
+
+		Util.debug1("enableDragDrop is TRUE, so adding DnD support in activity ");
+
+		setDragDrop(R.id.my_sky_layout, R.id.sun );  // background layout and element id to drag
+	}
+
+	private boolean isDragDropEnabled()
+	{
+		Bundle b = getIntent().getExtras();
+		boolean enableDragDrop = false;
+
+		Util.debug1("b = " + b);
+
+		if (b != null && b.containsKey(Util.DRAGDROP_BOOLEAN_KEY))
+		{
+			enableDragDrop = b.getBoolean(Util.DRAGDROP_BOOLEAN_KEY);
+		}
+
+		Util.debug1("enableDragDrop = " + enableDragDrop);
+
+		return enableDragDrop;
+	}
+
+	private void setDragDrop(int viewGroup, int elementToTrack)
+	{
+		setDragDropViewGroup(viewGroup);
+
+		setDragDropElement(elementToTrack);
+	}
+
+	private void setDragDropViewGroup (int viewGroup)
+	{
+		ViewGroup vg = (ViewGroup) findViewById(viewGroup);
+		vg.setOnTouchListener(new View.OnTouchListener()
+		{
+			@Override
+			public boolean onTouch(View v, MotionEvent event)
+			{
+				switch (event.getActionMasked())
+				{
+					case MotionEvent.ACTION_MOVE:
+						int x = (int) event.getX() - offset_x;
+						int y = (int) event.getY() - offset_y;
+
+						int w = getWindowManager().getDefaultDisplay().getWidth() - 100;
+						int h = getWindowManager().getDefaultDisplay().getHeight() - 100;
+						if (x > w)
+							x = w;
+						if (y > h)
+							y = h;
+
+						RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+								new ViewGroup.MarginLayoutParams(
+										RelativeLayout.LayoutParams.WRAP_CONTENT,
+										RelativeLayout.LayoutParams.WRAP_CONTENT));
+						lp.setMargins(x, y, 0, 0);
+
+						selected_item.setLayoutParams(lp);
+						break;
+					default:
+						break;
+				}
+				return true;
+			}
+		});
+	}
+
+	private void setDragDropElement(int elementToTrack)
+	{
+		ImageView img = (ImageView) findViewById(elementToTrack);
+		img.setOnTouchListener(new View.OnTouchListener()
+		{
+			@Override
+			public boolean onTouch(View v, MotionEvent event)
+			{
+				switch (event.getActionMasked())
+				{
+				case MotionEvent.ACTION_DOWN:
+					offset_x = (int) event.getX();
+					offset_y = (int) event.getY();
+					selected_item = v;
+					break;
+				default:
+					break;
+				}
+
+				return false;
+			}
+		});
+	}
+}
